dd6eec7a293450abebcab809780e4422fde85340
Nazmul-vs-code <khalekbiton1977@gmail.com>
2026-06-05 17:03:13 +0600

    connection based on company

--- src/app/dashboard/recruiter/jobs/page.jsx
+++ src/app/dashboard/recruiter/jobs/page.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import RecentApplications from '@/components/dashboard/RecentApplications';
 import TopCompanies from '@/components/dashboard/TopCompanies';
 import { getJobs } from '@/lib/api/jobs';
+import { GetLoggedinRecruiterCompany } from '@/lib/api/companys';
 
 const RecruiterJobsPage = async () => {
-  const companyId = 'company_id_xyz123';
-  const jobs = await getJobs(companyId) || [];
+  
+  const company = await GetLoggedinRecruiterCompany();
+
+  const jobs = await getJobs(company._id) || [];
 
   return (
     <div className="min-h-screen bg-[#020105] text-zinc-100 p-4 sm:p-6 lg:p-8">
--- src/lib/api/companys.js
+++ src/lib/api/companys.js
@@ -1,7 +1,14 @@
 import { serverFetch } from "../core/server";
+import { getUserSession } from "../core/session";
 
 const ServerUrl = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:5000';
 
 export const getRecruiterCompany = async (recruiterId) => {
     return serverFetch(`/api/my/companys?recruiterId=${recruiterId}`)
+}
+
+
+export const GetLoggedinRecruiterCompany = async () => {
+    const user = await getUserSession();
+    return getRecruiterCompany(user?.id)
 }
